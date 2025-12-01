033e4b0075f670dae793709bdefd68f99e4593d0
interesnij <ochkarik1983@mail.ru>
2025-12-01 18:00:29 +0300

    f

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -65,7 +65,7 @@ export const SignupPage: React.FC<RouteComponentProps> = () => {
       ]}
     >
       <div className="signup-page-account-type">Select Your Account Type</div>
-      <div>
+      <div display="flex">
       {accountData.map((account, index) => (
         <Account {...account} key={`${index}key`} />
       ))}
